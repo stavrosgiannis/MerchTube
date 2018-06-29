35bb2d7d1ccc813f42680b8fe89e1e6c58875f6e
ebayerl <ebayerl@onbtineuss.de>
2018-06-29 10:35:31 +0200

    bilder_einfügen
    bilder mit scrollbar eingefügt

--- PHPWebProject1/PHPWebProject1/_pages/index.php
+++ PHPWebProject1/PHPWebProject1/_pages/index.php
@@ -12,12 +12,17 @@ include '../_module/bottom.php';
 <div class="top-space">
 </div>
 
-<div class="mainspace">
-<a class="ueberschrift"><h2>#A</h2></a>
+<div class="container allign-container">
+<div class="reihe1">
+	<!-- Zahl = bild breite -->
+	<a class="ueberschrift" href="_pages/#A"><p>#A</p></a>
+	<!-- Name und Bilder nachträglich einfügen -->
+	<img class="container-bilder" src="../_img/test_bild.png" alt="test1"> </img>	
+	<img class="container-bilder" src="../_img/test_bild.png" alt="test2"> </img>	
+	<img class="container-bilder" src="../_img/test_bild.png" alt="test1"> </img>	
+	<img class="container-bilder" src="../_img/test_bild.png" alt="test2"> </img>
 
-
-
-
-
+	</div>
+</div>
 
 </div>
