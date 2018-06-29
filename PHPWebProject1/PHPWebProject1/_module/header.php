02d5748b12fe7c9e1745b5db5f7acdc6ca2730b4
Stav ros <hackerone1@web.de>
2018-06-29 13:36:52 +0200

    template

--- PHPWebProject1/PHPWebProject1/_module/header.php
+++ PHPWebProject1/PHPWebProject1/_module/header.php
@@ -11,7 +11,6 @@
 ?>
 
 <header>
-
     <div class="header-area">
         <div class="container">
             <div class="row">
--- PHPWebProject1/PHPWebProject1/_pages/index.php
+++ PHPWebProject1/PHPWebProject1/_pages/index.php
@@ -3,25 +3,60 @@
 include '../_module/top.php';
 ?>
 
-
-
 <div class="top-space"></div>
 
 <div class="container align-container">
     <div class="row">
-        <div class="col-md-4">
-            <div id="titel-container">
-                <a class="ueberschrift">
-                    <h2 id="sort_a">#A</h2>
-                </a>
-            </div>
-            <div id="bild-container">
+        <div id="bild-container" class="">
+            <!-- 3D Slideshow Section -->
+            <section id="slideshow">
 
-            </div>
+                <div class="entire-content">
+                    <div class="content-carrousel">
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/758733/pexels-photo-758733.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/21261/pexels-photo.jpg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/567973/pexels-photo-567973.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/776653/pexels-photo-776653.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/54630/japanese-cherry-trees-flowers-spring-japanese-flowering-cherry-54630.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/131046/pexels-photo-131046.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/302515/pexels-photo-302515.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/301682/pexels-photo-301682.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                        <figure class="shadow">
+                            <img src="https://images.pexels.com/photos/933054/pexels-photo-933054.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" />
+                        </figure>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <hr />
+        <div class="werbung-banner">
+            <h1>Banner-Werbung</h1>
+        </div>
+
+        <div id="titel-container">
+            <a style="text-decoration:none;" class="ueberschrift">
+                <h2 id="sort_a">#A</h2>
+            </a>
         </div>
     </div>
 </div>
-
 
 <?php
 // --------------------------------------------Bottom------------------------------------------------------------->
